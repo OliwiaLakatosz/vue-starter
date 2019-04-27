9ead1a66caaa83835c908b6222cae22f5958f028
Oliwia <olakatosz18@gmail.com>
2019-04-27 15:47:05 +0200

    list of meetings in progress

--- src/components/LoggedInPage.vue
+++ src/components/LoggedInPage.vue
@@ -2,11 +2,34 @@
 	<div>
 		<p>Zalogowany jako {{ username }}</p>
 		<button @click="$emit('logout')">Wyloguj sie</button>
+		
+		<h1>Spotkania</h1>
+		
+		<button @click="addNewMeeting()">Dodaj nowe spotkanie</button>
+		
+		<ol>
+  			<li v-for="meeting in meetings" :key="meeting.id">{{ meeting }}</li>
+		</ol>
+		
 	</div>
 </template>
 
 <script>
 	export default {
-		props: ['username']
+		props: ['username'],
+		data() {
+			return {
+				meetings: [
+					'MWO',
+					'TO',
+					'INNE',
+				]
+			};
+		},
+		methods: {
+			addNewMeeting(meeting) {
+				this.meetings.push("nowe spotkanie");
+			}
+		}
 	}
 </script>
